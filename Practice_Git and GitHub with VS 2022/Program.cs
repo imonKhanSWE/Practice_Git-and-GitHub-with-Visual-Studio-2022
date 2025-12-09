59d16f1a0b4c72dc8e71738e80d7e944320d8d1e
imon-khan <emon.khan.1573@gmail.com>
2025-12-09 14:45:05 +0600

    Add project files.

---
+++ Practice_Git and GitHub with VS 2022/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Git_and_GitHub_with_VS_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hellow world");
+        }
+    }
+}
